fb37b7a50f1c4ec72ada62f1a1c72dbc50b2d0be
SemperPhoenix <semperphoenixaz@gmail.com>
2018-01-16 20:05:27 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jason Mitchell on 1/16/18.
+//  Copyright © 2018 Jason Mitchell. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
