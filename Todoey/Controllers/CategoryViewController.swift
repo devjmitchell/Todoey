cc3bf7403af922938a0b480cfc14df29f50b4958
SemperPhoenix <semperphoenixaz@gmail.com>
2018-01-21 21:41:28 -0700

    Link Category View Controller to Todo List View Controller - still need to fix a bug

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -37,6 +37,20 @@ class CategoryViewController: UITableViewController {
         return cell
     }
     
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
     //MARK: - Data Manipulation Methods
     
     func saveCategories() {
@@ -95,8 +109,5 @@ class CategoryViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
         
     }
-    
-    //MARK: - TableView Delegate Methods
-    
     
 }
